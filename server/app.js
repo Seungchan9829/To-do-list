859c33233febc1ee573d24f1bf705a89db4ccabc
Seungchan9829 <def677kr@gmail.com>
2024-02-19 16:27:24 +0900

    데이터 베이스 연결 및 추가 삭제 기능 추가

---
+++ server/app.js
@@ -0,0 +1,101 @@
+const express = require('express')
+const app = express()
+const port = 4000
+const cors = require('cors');
+const pg = require('pg');
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+const conString = "postgres://szvwidna:ScLy3yxV3D93Y3qsKI7-QDf544LHc2gH@arjuna.db.elephantsql.com/szvwidna"
+const client = new pg.Client(conString);
+
+client.connect(function(err) {
+  if (err) {
+    return console.error('could not connect to postgres', err);
+  }
+  console.log('Connected to PostgreSQL database');
+
+  // client.end(); // Don't end the client here if you need it for subsequent queries
+});
+
+app.get('/task', (req, res) => {
+  client.query('SELECT * FROM "to-do-list"."users"', function(err, result) {
+    if (err) {
+      return console.error('error running query', err);
+    }
+    res.json(result.rows); // Only send the rows as JSON
+    console.log(result.rows);
+  });
+});
+
+
+// 데이터 베이스에서 할 일을 가져오는 API
+app.get('/tasks/:type', (req,res) => {
+  // :type는 동적 매개변수로 취급 된다. req.params.type은 :type으로 대치된다.
+  const taskType = req.params.type;
+  // === 비교 연산자임
+  if( taskType === 'exercise'){
+    client.query('SELECT todo_id, todo_contents, todo_completed, category_id FROM "to-do-list"."todo" where category_id = 1 ORDER BY todo_id;', function(err, result){
+      if(err){
+        return console.error('error running query', err);
+      }
+      // 배열을 반환한다.
+      res.json(result.rows);
+      console.log(result.rows)
+    })
+  }
+  else {
+    client.query('SELECT todo_id, todo_contents, todo_completed, category_id FROM "to-do-list"."todo" where category_id = 2 ORDER BY todo_id;', function(err, result){
+      if(err){
+        return console.error('error running query', err);
+      }
+      // 배열을 반환한다.
+      res.json(result.rows);
+      console.log(result.rows)
+    })
+  }
+})
+
+app.post('/task/:type', async (req, res) => {
+  const { todo_contents, todo_completed, category_id} = req.body;
+  
+  try{
+    const result = await client.query('INSERT INTO "to-do-list"."todo" (todo_contents, todo_completed, category_id) VALUES ($1, $2, $3)',
+    [todo_contents, todo_completed, category_id])
+
+    res.send("데이터베이스 저장 완료")
+  }
+  catch(error){
+    console.error('error',error);
+
+  }
+});
+
+app.put('/task/:type', async (req,res) => {
+  const {todo_id} = req.body;
+  res.send(`${todo_id}`);  
+  try{
+    await client.query(`UPDATE "to-do-list"."todo" SET todo_completed = NOT todo_completed WHERE todo_id = ${todo_id}`)
+
+  }
+  catch(error){
+    console.error(error);
+  }
+})
+
+app.delete('/task/:type/:todo_id', async (req,res) => {
+  const todo_id = req.params.todo_id;
+  console.log(`${todo_id}`)
+  try{
+    await client.query(`DELETE FROM "to-do-list"."todo" WHERE todo_id = ${todo_id}`)
+  }
+  catch(error){
+    console.error(error);
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
--- todolist/src/list.js
+++ todolist/src/list.js
@@ -1,16 +1,18 @@
 import {React, useEffect, useState} from'react';
+import axios from "axios";
 
 export default function List({type}){
     
+
     const [tasks, setTasks] = useState([]);
-    const [completedTasks, setCompletedTasks] = useState([]);
+    
+    const [taskCount, setTaskCount] = useState(0);
     // 로컬 스토리지에서 데이터를 가지고 옵니다
     // 렌더링 될 때 한번만 실행 빈 배열이기 떄문
     useEffect(()=> {
-        const storedTasks = JSON.parse(localStorage.getItem(`tasks_${type}`)) || [];
-        setTasks(storedTasks);
-        const storedCompletedTasks = JSON.parse(localStorage.getItem(`completedTasks_${type}`)) || [];
-        setCompletedTasks(storedCompletedTasks);
+
+        getTask(); // 할일 가져오기
+  
     },[])
 
     const [newTask, setNewTask] = useState('');
@@ -21,51 +23,111 @@ export default function List({type}){
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
     const formattedDate = currentDate.toLocaleDateString('ko-KR', options);
     
-    // 
-    
-    let count = completedTasks.filter((task,index) => !completedTasks[index]).length;
     
     //작업을 추가합니다.
-    const addTask = ()=>{
+    const addTask = async ()=>{
         if(newTask.trim() !== '') {
             // 새로운 할 일을 추가합니다.
+            const transferData = {
+                todo_contents : `${newTask}`,
+                todo_completed : false,
+                category_id : type === 'exercise' ? 1 : 2
+            };
+
             setTasks([...tasks, newTask]);
-            localStorage.setItem(`tasks_${type}`, JSON.stringify([...tasks, newTask]))
+            await createTask(transferData);
+               
             setNewTask('');
-            setCompletedTasks([...completedTasks, false]);
-            localStorage.setItem(`completedTasks_${tasks}`, JSON.stringify([...completedTasks, false]));
+            getTask();
+            
         }
     };
     
     // 할일을 완료하면 문장 가운데 줄을 긋습니다.
-    const toggle = (index) => {
-        const newCompltedTasks = [...completedTasks];
-        newCompltedTasks[index] = !newCompltedTasks[index];
-        setCompletedTasks(newCompltedTasks);
-        localStorage.setItem(`completedTasks_${tasks}`, JSON.stringify(newCompltedTasks));
+    const toggle = async (index) => {
+        const newCompltedTasks = [...tasks];
+        newCompltedTasks[index].todo_completed = !newCompltedTasks[index].todo_completed;
+        setTasks(newCompltedTasks);
+        // 할일 개수 업데이트
+        let count = newCompltedTasks.filter((task => !task.todo_completed)).length;
+            setTaskCount(count);
+        // 업데이트를 하는 코드를 작성하자.
+        const data = {
+            todo_id : tasks[index].todo_id
+        }
+
+        await axios.put(`http://localhost:4000/task/${type}`, data);
+
+
     }
-    const removeTask = (index)=>{
+
+    const removeTask = async (index)=>{
         // 변수 선언
-        const newTasks = [...tasks];
-        const newCompltedTasks = [...completedTasks];
-        // 삭제 
-        newTasks.splice(index,1);
-        newCompltedTasks.splice(index,1);
-        // 변수 재설정
-        setTasks(newTasks);
-        localStorage.setItem(`tasks_${tasks}`, JSON.stringify(newTasks));
+        const deleteTasks = [...tasks];
+        
+        const data = {
+            todo_id : deleteTasks[index].todo_id
+        }
+        
+        deleteTasks.splice(index, 1);
+        setTasks(deleteTasks);
 
-        setCompletedTasks(newCompltedTasks);
-        localStorage.setItem(`completedTasks_${tasks}`, JSON.stringify(newCompltedTasks))
+        let count = deleteTasks.filter((task => !task.todo_completed)).length;
+        setTaskCount(count);
+
+        await deleteTask(data);
+           
+
     }
 
+
+    // 데이터 베이스에서 할일 가져오는 함수
+    async function getTask(){
+        try{
+            const response = await axios.get(`http://localhost:4000/tasks/${type}`);
+            const storedtask = response.data || [];
+            setTasks(storedtask);
+        
+            // 할 일 개수 확인  true의 개수 true면 할일 완료
+            let count = storedtask.filter((task => !task.todo_completed)).length;
+            setTaskCount(count);
+
+        }
+        catch(error) {
+            console.error(error);
+        }
+    }
+    // 데이터 베이스에 데이터 추가
+    async function createTask(datas){
+        console.log(datas)
+        try{
+            await axios.post(`http://localhost:4000/task/${type}`, datas);
+
+        }
+        catch(error){
+            console.error(error);
+        }
+    }
+    // 데이터 베이스 삭제
+    async function deleteTask({todo_id}){
+     
+        try{
+            
+            await axios.delete(`http://localhost:4000/task/${type}/${todo_id}`)
+    
+            
+        }
+        catch(error){
+            console.error(error);
+        }
+    }
     return(
         <>
         
         <div className="container">
             <h1>{formattedDate}</h1>
             <div className="day">{datofweek}요일</div>
-            <div className="task-left">할 일 {count}개 남음</div>
+            <div className="task-left">할 일 {taskCount}개 남음</div>
         </div>
 
         <div className="todo-container">
@@ -79,16 +141,21 @@ export default function List({type}){
             <div className="todolist">
                 {tasks.map((task, index) => (
                     <div className="todoItem">
-                    <input type="checkbox" className = "checkbox" checked={completedTasks[index]} onChange={()=>toggle(index)}/>
+                    <input type="checkbox" className = "checkbox" checked={task.todo_completed} onChange={()=>toggle(index)}/>
                     <li key ={index}>
                         
-                        <h2 className={`${completedTasks[index] ? 'completed' : ''}`}>{task}</h2>
+                        <h2 className={`${task.todo_completed ? 'completed' : ''}`}>{task.todo_contents}</h2>
                     </li>
                     <button onClick={()=> removeTask(index)}>삭제</button>
                     </div>
                 ))}
             </div>
+
+            
+            
+
         </div>
         </>
     )
 }
+
